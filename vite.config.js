e475164e6869e4ae98c526d2e84308f835c0cee7
antonious nasr <antoniousnasr3@gmail.com>
2025-03-08 00:25:26 +0200

    new tailwind css

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import eslint from "vite-plugin-eslint";
+import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), eslint()],
+  plugins: [react(), eslint(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
